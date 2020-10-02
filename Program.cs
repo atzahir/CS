7633e63a9ad044d1ca55df45286fa549c3e5aa5b
Akhmad Taajuddin Zahir <ahmadtajudin2930@gmail.com>
2020-10-02 13:56:53 +0700

    initialization commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aktifitas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aha, ternyata coding C# mudah!");
+            Console.WriteLine("Coba tekan tombol apapun selain enter. Lihat!");
+            Console.ReadKey();
+        }
+    }
+}
